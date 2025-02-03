97bf9a35c3b28232faed76ccbf979495a2416847
akinblaq <ogundelekn@gmail.com>
2025-02-03 12:24:08 +0100

    NavBar resolution

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Navbar from '../components/NavBar/Navbar'
-import { Outlet } from 'react-router-dom'
-import Footer from '../components/Footer/Footer'
+import React from "react";
+import NavBar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
 
 const RootLayout = () => {
   return (
     <div>
-        <Navbar />
-        <Outlet />
-        <Footer />
+      <NavBar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
